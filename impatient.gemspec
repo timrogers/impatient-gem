32dfea41b04807447e1fd405584249566eef6f91
Tim Rogers <tim@tim-rogers.co.uk>
2012-08-08 11:21:13 +0100

    Initial commit of 1.0.0

---
+++ impatient.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'impatient'
+  s.version     = '1.0.0'
+  s.date        = '2012-08-08'
+  s.summary     = "A quick command to let you know when a Heroku app is ready to go"
+  s.description = "Some of us are impatient. At least, I am - when I deploy an update to my app to Heroku, I want to know whether it's ready. This tells you."
+  s.authors     = ["Tim Rogers"]
+  s.email       = 'tim@gocardless.com'
+  s.files       = ["lib/impatient.rb"]
+  s.executables << "imp"
+  s.homepage    =
+    'https://github.com/timrogers/impatient-gem'
+end
---
+++ lib/impatient.rb
@@ -0,0 +1,32 @@
+class Impatient
+  VERSION = "1.0.0"
+  
+  def self.execute(open_when_ready = false)
+    ready = false
+    while !ready do
+      print "."
+      ps = `heroku ps`
+      ready = true if !ps.include?("starting")
+    end
+    
+    puts "\n\n"
+    if ps.include?("up")
+      puts "The app is ready to go:\n\n"
+      puts ps
+      if open_when_ready
+        `heroku open`
+        puts "\nLaunching app..."
+      end
+    else
+      puts "Something went wrong when the app was launching - the process responded:\n\n"
+      puts ps
+      puts "\n\nFetching logs from Heroku...\n\n"
+      print `heroku logs`
+    end
+  end
+  
+  def self.version
+    "1.0.0"
+  end
+  
+end
